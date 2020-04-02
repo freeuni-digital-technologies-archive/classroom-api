5d92d9c0bc512033a9c41c4c0ca9d5d65aab1041
iarigby <marvinzem@gmail.com>
2020-04-03 00:40:40 +0400

    start config files

---
+++ config/homeworks.ts
@@ -0,0 +1,12 @@
+import { HomeWork } from '../src/types'
+
+export const homeworks: HomeWork[] = [
+    {
+        name: 'დავალება 1',
+        id: 'hw1'
+    },
+    {
+        name: 'ბონუსი 1',
+        id: 'bonus1'
+    }
+]
